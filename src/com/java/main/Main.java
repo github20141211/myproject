74f7c7ab309dcc2791c6580983abfb03911a1e9e
luocheng2015 <444790866@qq.com>
2017-07-10 14:18:37 +0800

    初始化项目

---
+++ src/com/java/main/Main.java
@@ -0,0 +1,13 @@
+package com.java.main;
+
+/**
+ * 功能描述
+ * 创建人 luocheng
+ * 创建时间2017/7/10.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello main... git");
+    }
+}
