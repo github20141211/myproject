bd1fe4a1aaec67f0ff4447453dcc2a62b60b5f8b
luocheng2015 <444790866@qq.com>
2017-07-10 23:53:00 +0800

    初始化项目

--- src/com/java/main/Main.java
+++ src/com/java/main/Main.java
@@ -9,5 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello main... git");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
